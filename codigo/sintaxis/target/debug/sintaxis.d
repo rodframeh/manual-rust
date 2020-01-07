ce8e8d3498dca047a5c8ebeebb983ae485acd1e7
rodframeh <rodframeh@gmail.com>
2020-01-07 09:41:19 -0500

    se agrego introduccion, sintaxis e instalacion

---
+++ codigo/sintaxis/target/debug/sintaxis.d
@@ -0,0 +1,1 @@
+/home/rodframeh/Raiz/Computación/Lenguaje\ Rust/manual-rust/codigo/sintaxis/target/debug/sintaxis: /home/rodframeh/Raiz/Computación/Lenguaje\ Rust/manual-rust/codigo/sintaxis/src/main.rs
