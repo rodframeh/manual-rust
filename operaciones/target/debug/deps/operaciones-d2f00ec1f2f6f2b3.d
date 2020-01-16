4410b8335761b0cbfc185c9c90108e5caf97aa52
rodframeh <rodframeh@gmail.com>
2020-01-16 08:37:41 -0500

    se añadio codigo a operaciones

---
+++ operaciones/target/debug/deps/operaciones-d2f00ec1f2f6f2b3.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/manual-rust/operaciones/target/debug/deps/operaciones-d2f00ec1f2f6f2b3: src/main.rs
+
+/data/data/com.termux/files/home/manual-rust/operaciones/target/debug/deps/operaciones-d2f00ec1f2f6f2b3.d: src/main.rs
+
+src/main.rs:
---
+++ operaciones/target/debug/operaciones.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/manual-rust/operaciones/target/debug/operaciones: /data/data/com.termux/files/home/manual-rust/operaciones/src/main.rs
